f1c19d4cf566aaf79d346b2242dd628decbf7d13
s0larish <risht42@gmail.com>
2025-08-13 10:38:18 -0600

    created __init__.py

---
+++ __init__.py
@@ -0,0 +1,14 @@
+"""
+sirgraf package
+
+"""
+
+from .background import (
+    create_static_background,
+    create_uniform_background
+)
+
+__all__ = [
+    "create_static_background",
+    "create_uniform_background"
+]
